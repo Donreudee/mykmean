dfc1376f2b66d9d100abf48383317f80c20fac6e
Jelin <6531501042@lamduan.mfu.ac.th>
2025-04-20 17:03:40 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -14,12 +14,13 @@ from sklearn.datasets import make_blobs
 with open ('kmeans_model.pkl', 'rb') as f :
     loaded_model = pickle.load(f)
     
+# Set the page config
+st.set_page_config(page_title="k-means Clustering App", layout="centered")
 
 # Set title
 st.title(" k-Means Clustering Visualizer by Dondi Tonphot")
 
-# Set the page config
-st.set_page_config(page_title="k-means Clustering App", layout="centered")
+
 
 #load dataset
 X, _ = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
@@ -27,7 +28,7 @@ X, _ = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
 y_kmeans = loaded_model.predict(X)
 
 #plotting
-fig, ax = plt.subplot()
+fig, ax = plt.subplots()
 plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
 ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
 plt.title('k-Means Clustering')
